e0184775c524a55c91fa75df8dd031622f7d802a
Joe <joethuo6@gmail.com>
2017-11-23 09:27:54 +0300

    Revert "create-user endpoint"
    This reverts commit f85f49c89dcaf3b68e0cd4dfb2e88a8a59152cde.

--- create-user.py
+++
@@ -1,6 +0,0 @@
-from app import app
-from flask import Flask, render_template
-
-@app.route('/api/auth/register', methods = ['POST'])
-def create_account():
-    return render_template('create-user.html')
